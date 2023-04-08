7ad27d9bb91281af159c13a53118d97d48da5dd9
Mkdadwal <107403227+Mkdadwal@users.noreply.github.com>
2023-04-09 01:32:02 +0530

    First Commit

---
+++ DrawerNavigation/app/src/main/java/com/example/drawernavigation/LoginActivity.kt
@@ -0,0 +1,38 @@
+package com.example.drawernavigation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import kotlin.math.log
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var username : EditText
+    lateinit var password : EditText
+    lateinit var login : Button
+    lateinit var regex : Regex
+    lateinit var register : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        login = findViewById(R.id.btnLogin)
+        register = findViewById(R.id.btnRegister)
+        login.setOnClickListener {
+            regex = Regex("""\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}""")
+            if(android.util.Patterns.EMAIL_ADDRESS.matcher(username.text).matches()){
+                Toast.makeText(this,"Login Successfull", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"Invalid Email", Toast.LENGTH_SHORT).show()
+            }
+        }
+        register.setOnClickListener {
+            var intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
